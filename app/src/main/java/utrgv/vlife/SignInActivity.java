431115df81f3766e55bc3651e59cc2d0c41cad50
davelasquez12 <davelasquez12@gmail.com>
2016-12-02 02:40:43 -0600

    -Working on fixing the toolbar and menu in MainActivity

--- app/src/main/java/utrgv/vlife/SignInActivity.java
+++ app/src/main/java/utrgv/vlife/SignInActivity.java
@@ -1,15 +1,32 @@
 package utrgv.vlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity
 {
-
+	private Button mSignInButton;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_in);
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+		mSignInButton = (Button) findViewById(R.id.sign_in_button);
+
+		mSignInButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				Intent i = new Intent(SignInActivity.this, MainActivity.class);
+				startActivity(i);
+			}
+		});
 	}
 }
