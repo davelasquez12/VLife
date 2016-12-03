37debf3848afd13bebcdfda8f3bf791173f4744f
David Velasquez <davelasquez12@gmail.com>
2016-12-03 07:13:35 -0600

    -Added menu icon (hamburger icon) to MainActivity

--- app/src/main/java/utrgv/vlife/MainActivity.java
+++ app/src/main/java/utrgv/vlife/MainActivity.java
@@ -1,17 +1,25 @@
 package utrgv.vlife;
 
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
 {
 	private Toolbar mToolbar;
 	private MainVPAdapter mMainVPAdapter;
 	private ViewPager mViewPager;
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private ListView mNavDrawerLV;
 	private int mCurrentVPPos = MainVPAdapter.NUM_EVENT_FRAGS/2;
 
 	@Override
@@ -21,8 +29,11 @@ public class MainActivity extends AppCompatActivity
 		setContentView(R.layout.activity_main);
 
 		mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+		mNavDrawerLV = (ListView) findViewById(R.id.main_nav_drawer_list_view);
 		mViewPager = (ViewPager) findViewById(R.id.main_view_pager);
 		setSupportActionBar(mToolbar);
+		setupNavDrawer();
 		setupViewPager();
 	}
 
@@ -32,6 +43,27 @@ public class MainActivity extends AppCompatActivity
 		MenuInflater menuInflater = getMenuInflater();
 		menuInflater.inflate(R.menu.main_toolbar_menu, menu);
 		return true;
+	}
+
+	private void setupNavDrawer()
+	{
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.nav_drawer_opened, R.string.nav_drawer_closed)
+		{
+			@Override
+			public void onDrawerOpened(View drawerView)
+			{
+				super.onDrawerOpened(drawerView);
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView)
+			{
+				super.onDrawerClosed(drawerView);
+			}
+		};
+
+		mDrawerLayout.addDrawerListener(mDrawerToggle);
+		mDrawerToggle.syncState();
 	}
 
 	private void setupViewPager()
@@ -45,7 +77,6 @@ public class MainActivity extends AppCompatActivity
 			@Override
 			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels)
 			{
-
 			}
 
 			@Override
@@ -60,6 +91,5 @@ public class MainActivity extends AppCompatActivity
 
 			}
 		});
-
 	}
 }
